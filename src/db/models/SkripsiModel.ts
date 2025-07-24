3c5048e9cc35fe1c59a0a1a47a6b0cf582191d2c
Abdul <hafizhahm123@gmail.com>
2025-07-24 10:19:15 +0700

    refactor: type thesis

--- src/db/models/SkripsiModel.ts
+++
@@ -1,9 +0,0 @@
-import {db} from "@/db/config/mongodb"
-
-class SkripsiModel {
-    static async collection() {
-        return await db("skripsi")
-    }
-}
-
-export default SkripsiModel
---
+++ src/libs/schemas/ThesisSchema.ts
@@ -0,0 +1,19 @@
+import * as z from "zod";
+
+const thesisSchema = z.object({
+  id: z.string(),
+  judul: z.string({
+    required_error: "Judul harus diisi!",
+  }),
+  abstrak: z.string().optional(),
+  nim: z.string({
+    required_error: "NIM harus diisi"
+  }),
+  tahun: z.string({
+    required_error: 'Tahun harus diisi'
+  }),
+  createdAt: z.string(),
+  updatedAt: z.string(),
+});
+
+export default thesisSchema;
