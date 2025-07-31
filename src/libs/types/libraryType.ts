c08b1f3bf41f2eb14c58dc927633dece7e3cb408
Abdul <hafizhahm123@gmail.com>
2025-07-31 12:28:21 +0700

    refactor: refactor type library

--- src/libs/types/libraryType.ts
+++ src/libs/types/libraryType.ts
@@ -1,4 +1,4 @@
-import { Penerbit, Pengarang } from "../types";
+import { Mahasiswa, Penerbit, Pengarang, Publikasi } from "../types";
 
 export interface BaseItem {
   _id?: { $oid: string } | string;
@@ -24,12 +24,14 @@ export interface Book extends BaseItem {
 export interface Journal extends BaseItem {
   jurnal_id: string;
   type: "journal";
+  publikasi: Publikasi;
 }
 
 export interface Skripsi extends BaseItem {
   tahun: string;
   nim: string;
   type: "skripsi";
+  mahasiswa: Mahasiswa;
 }
 
 export type LibraryItem = Book | Journal | Skripsi;
