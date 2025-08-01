1dbf1e41bd891e73e1e88a79278c0099585846e4
Abdul <hafizhahm123@gmail.com>
2025-08-01 08:26:51 +0700

    refactor: bubble journal and book

--- src/components/library/BookAndJournalDisplayChat.tsx
+++ src/components/library/BookAndJournalDisplayChat.tsx
@@ -1,28 +1,50 @@
-import { ResultChatbotCard } from "@/libs/types";
+import { Book, Journal } from "@/libs/types/libraryType";
 
 const BookAndJournalBubbleChat: React.FC<{
-  item: ResultChatbotCard;
+  item: Book | Journal;
   type: string | undefined;
 }> = ({ item, type }) => {
   const itemType = type === "buku" ? "Buku" : "Jurnal";
   return (
     <div className="mt-5 rounded-bl-none bg-blue-200 p-5 text-black">
-      <h3>
-        Judul: <span className="font-bold">{item?.judul || ""}</span>{" "}
-      </h3>
-      <p>Sinopsis: {item?.abstrak || ""}</p>
-      <p>Jumlah: {item?.jumlah || ""}</p>
-      <p>
-        {itemType} tersedia: {item?.tersedia || ""}
-      </p>
-      <p>
-        {itemType} sedang dipinjam: {item?.dipinjam || ""}
-      </p>
-      {type === "buku" && (
+      {type === "buku" ? (
         <>
-          <p>Pengarang: {item?.pengarang?.name || ""}</p>
-          <p>Penerbit: {item?.penerbit?.name || ""}</p>
-          <p>Lokasi buku: Rak {item?.id || ""}</p>
+          {/* tipe item === buku */}
+          <h3>
+            Judul:{" "}
+            <span className="font-bold">
+              {(item as Book)?.judul || ""}
+            </span>{" "}
+          </h3>
+          <p>Sinopsis: {(item as Book)?.sinopsis}</p>
+          <p>Pengarang: {(item as Book)?.pengarang?.name || ""}</p>
+          <p>Penerbit: {(item as Book)?.penerbit?.name || ""}</p>
+          <p>Lokasi buku: {(item as Book)?.lokasi || ""}</p>
+          <p>Rak: {(item as Book)?.rak || ""}</p>
+        </>
+      ) : (
+        <>
+          {/* tipe item === jurnal */}
+          <h3>
+            Judul:{" "}
+            <span className="font-bold">
+              {(item as Journal)?.judul || ""}
+            </span>{" "}
+          </h3>
+          <p>Sinopsis: {(item as Journal)?.abstrak || ""}</p>
+
+          <p>
+            Publikasi: {(item as Journal)?.publikasi?.name || ""},{" "}
+            {(item as Journal)?.publikasi?.tahun || ""},{" "}
+            {(item as Journal)?.publikasi?.volume || ""}
+          </p>
+          <p>Jumlah: {(item as Journal)?.jumlah || ""}</p>
+          <p>
+            {itemType} tersedia: {(item as Journal)?.tersedia || ""}
+          </p>
+          <p>
+            {itemType} sedang dipinjam: {item?.dipinjam || ""}
+          </p>
         </>
       )}
     </div>
